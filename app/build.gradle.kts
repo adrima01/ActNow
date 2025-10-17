6b9d9ac215e65b852a73dd4913e9b69edfe883db
Adrian Maraj <adrian.maraj@yahoo.de>
2025-10-17 10:50:41 +0200

    changing minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.actnow"
-        minSdk = 34
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
