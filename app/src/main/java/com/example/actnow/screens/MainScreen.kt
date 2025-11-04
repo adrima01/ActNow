ea69e69b1f43167cbeafdfe9ca432d5dac72d3e0
Lynn <lynn@example.com>
2025-11-04 20:14:51 +0100

    Modification de MissionCard, MissionSearch, missionScreen et ajout de DetailsScreen, ParticiperButton et images des participants

--- app/src/main/java/com/example/actnow/screens/MainScreen.kt
+++ app/src/main/java/com/example/actnow/screens/MainScreen.kt
@@ -9,11 +9,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Map
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.actnow.missionData
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val navController = rememberNavController()
     var selectedItem by remember { mutableStateOf(0) }
 
     val items = listOf(
@@ -33,7 +38,16 @@ fun MainScreen() {
                 items.forEachIndexed { index, item ->
                     NavigationBarItem(
                         selected = selectedItem == index,
-                        onClick = { selectedItem = index },
+                        onClick = {
+                            selectedItem = index
+                            when (index) {
+                                0 -> navController.navigate("missions") {
+                                    popUpTo("missions") { inclusive = true }
+                                }
+                                1 -> navController.navigate("map")
+                                2 -> navController.navigate("profile")
+                            }
+                        },
                         icon = { Icon(item.icon, contentDescription = item.title) },
                         label = { Text(item.title) }
                     )
